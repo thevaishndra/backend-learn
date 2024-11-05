d622a2b0112bf3b6992a145189a2303f7ebc7685
thevaishndra <chandrav306m@gmail.com>
2024-11-05 23:06:51 +0530

    postman logic

--- 04project/src/controllers/user.controller.js
+++ 04project/src/controllers/user.controller.js
@@ -4,6 +4,18 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: "ok"
     })
+    //get user details from frontend
+    //validation - not empty
+    //check if user already exists: username, email
+    //check for images, check for avatar
+    //upload them to cloudinary
+    //create user object - create entry in db
+    //remove password and refresh token field from response
+    //check for user creation
+    //return res
+
+    const {fullname, username, email, password} = req.body
+    console.log("email: ", email);
 })
 
 export {registerUser}
