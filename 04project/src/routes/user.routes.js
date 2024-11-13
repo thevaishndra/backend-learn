291a3f6170d0a241549219c0be9119fdf22007cd
thevaishndra <chandrav306m@gmail.com>
2024-11-13 13:17:20 +0530

    access token refresh token

--- 04project/src/routes/user.routes.js
+++ 04project/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser, logoutUser } from "../controllers/user.controller.js";
 import {upload} from '../middlewares/multer.middleware.js';
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 router.route("/register").post(
@@ -15,6 +16,11 @@ router.route("/register").post(
         }
     ]),
     registerUser)
-// router.route("/login").post(login);
+
+router.route("/login").post(loginUser);
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router;
