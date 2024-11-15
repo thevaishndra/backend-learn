0c78075ad6b8f842021a2ff0d0b8e8df4d883a79
thevaishndra <chandrav306m@gmail.com>
2024-11-15 15:33:17 +0530

    subscription

--- 04project/src/controllers/user.controller.js
+++ 04project/src/controllers/user.controller.js
@@ -237,7 +237,7 @@ const changeCurrentPassword = asyncHandler(async(req, res) => {
 const getCurrentUser = asyncHandler(async(req, res) => {
   return res
   .status(200)
-  .json(200, req.user, "Current user fetched successfully")
+  .json(new ApiResponse(200, req.user, "Current user fetched successfully"))
 }) 
 
 const updateAccountDetails = asyncHandler(async(req, res) => {
@@ -270,13 +270,15 @@ const updateUserAvatar = asyncHandler(async(req, res) => {
     throw new ApiError(400, "avatar file is missing")
   }
 
+  //delete old image
+
   const avatar = await uploadCloudinary(avatarLocalPath)
 
   if(!avatar.url){
     throw new ApiError(400, "Error while uploading avatar")
   }
 
-  await User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set : {
@@ -285,9 +287,56 @@ const updateUserAvatar = asyncHandler(async(req, res) => {
     },
     {new : true}
   ).select('-password')
+
+   return res
+     .status(200)
+     .json(new ApiResponse(200, user, "avatar updated successfully"));
 })
 
-export {registerUser, loginUser, logoutUser, 
-  refreshAccessToken, changeCurrentPassword, getCurrentUser,
-updateAccountDetails, updateUserAvatar}
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+  const coverImageLocalPath = req.file?.path;
+
+  if (!coverImageLocalPath) {
+    throw new ApiError(400, "Cover image file is missing");
+  }
+
+  const coverImage = await uploadCloudinary(coverImageLocalPath);
+
+  if (!coverImage.url) {
+    throw new ApiError(400, "Error while uploading avatar");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        coverImage: coverImage.url,
+      },
+    },
+    { new: true }
+  ).select("-password");
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200, user, "cover image updated successfully")
+  )
+});
+
+const getUserChannelProfile = asyncHandler(async(req, res) => {
+  const {username} = req.params
+})
+
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile
+};
 
